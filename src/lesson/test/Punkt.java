0bfb71feb1551a6e0d83423c418b170c046941b4
oliver <Oliver@dokoliho.de>
2015-04-21 07:56:45 +0200

    Blatt 3 und Code zur Testvorlesung

---
+++ src/lesson/test/Punkt.java
@@ -0,0 +1,18 @@
+package lesson.test;
+
+// Klasse findet bei Folie 29ff Verwendung
+
+public class Punkt {
+	
+	private int x;
+	private int y;
+	
+	public Punkt(int newX, int newY) {
+		x = newX;
+		y = newY;
+	}
+
+	public int getX() { return x; }
+	public int getY() { return y; }
+	
+}
