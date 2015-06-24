50009c9227cb03f40ff16c5dff96e24a3fa2204f
Oliver Hofmann <oliver.hofmann@fhws.de>
2015-06-24 08:51:51 +0200

    Tennis final

--- src/lesson/tennis/Tennis.java
+++ src/lesson/tennis/Tennis.java
@@ -2,58 +2,85 @@ package lesson.tennis;
 
 public class Tennis {
 	
-	int LOVE = 0;
-	int FIFTEEN = 1;
-	int THIRTY = 2;
-	int FORTY = 3;
-	int ADVANTAGE = 4;
-	int WON = 6;
-	String[] points = {"Love", "15", "30", "40", 
-			"Vorteil", "60", "Gewonnen"};
+	final int LOVE = 0;
+	final int FIFTEEN = 1;
+	final int THIRTY = 2;
+	final int FORTY = 3;
+	final int ADVANTAGE = 4;
+	final int WON = 6;
+	String[] points = {"Love", "15", "30", "40"};
 	
 	int pointsOfA = LOVE;
 	int pointsOfB = LOVE;
 	
 	
-	int pointForX(int pointsOfX, int pointsOfY)
-	{
-		if (pointsOfX == FORTY)
-			if (pointsOfY < FORTY) {
-				return WON;
-			}
-		if (pointsOfX == ADVANTAGE) {
-			return WON;
-		}
-		return pointsOfX+1;
-		
-	}
-	
 	
 	void pointForA()
 	{
-		pointsOfA = pointForX(pointsOfA, pointsOfB);
+		pointsOfA++; 
 	}
 	
 	void pointForB()
 	{
-		pointsOfB = pointForX(pointsOfB, pointsOfA);
+		pointsOfB++;
 	}
 	
 	public String toString()
 	{
-		return "Ergebnis " + points[pointsOfA] + ":" + points[pointsOfB];
+		if (hasAWon()) return "A hat gewonnen";
+		if (hasBWon()) return "B hat gewonnen";
+		if ( hasXAdvantage(pointsOfA, pointsOfB) ) return "Vorteil A";
+		if ( hasXAdvantage(pointsOfB, pointsOfA) ) return "Vorteil B";
+		if ( isDeuce(pointsOfA, pointsOfB) ) return "Einstand";
+		return points[pointsOfA] + ":" + points[pointsOfB];
 	}
 
+	private boolean isDeuce(int x, int y)
+	{
+		if ( (x >= FORTY)  && (x == y) )
+			return true;
+		return false;		
+	}
+
+	
+	private boolean hasXAdvantage(int x, int y)
+	{
+		if ( (x > FORTY)  && (x >y) )
+			return true;
+		return false;		
+	}
+	
+	
+	private boolean hasXWon(int x, int y)
+	{
+		if ( (x > FORTY)  && (x >= y+2) )
+			return true;
+		return false;		
+	}
+	
+	private boolean hasAWon() {
+		return hasXWon(pointsOfA, pointsOfB);
+	}
+
+	
+	private boolean hasBWon() {
+		return hasXWon(pointsOfB, pointsOfA);
+	}
+
+	
 	public static void main(String[] args) {
 
 		Tennis t = new Tennis();
 		
-		t.pointForA();
-		t.pointForB();
-		t.pointForB();
-		t.pointForB();
-		t.pointForB();
-		System.out.println(t.toString());
+		while (!(t.hasAWon() || t.hasBWon()))
+		{
+			if (Math.random() < 0.5)
+				t.pointForA();
+			else
+				t.pointForB();
+			System.out.println(t.toString());			
+		}
+		
 		
 	}
 
