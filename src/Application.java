d6b0ccd7c3b1e7e42e90bbe174b2e12664366dc0
Oliver Hofmann <Oliver.Hofmann@dokoliho.de>
2015-10-08 12:47:05 +0200

    Clean Up

--- src/Application.java
+++ src/Application.java
@@ -1,13 +1,7 @@
-
-import java.util.Scanner;
-
 public class Application {
 	
   public static void main(String[] args) {
-	  
-    Scanner scanner = new Scanner(System.in);
-    int i = scanner.nextInt();
-    System.out.println(i*i);
-    scanner.close();
+    System.out.println("Hello World");
   }
+
 }
