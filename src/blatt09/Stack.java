b81d751074df8058f1239b33fe5e0d4f9893e88a
Oliver Hofmann <Oliver.Hofmann@dokoliho.de>
2015-12-07 11:40:58 +0100

    blatt09

---
+++ src/blatt09/Stack.java
@@ -0,0 +1,36 @@
+package blatt09;
+
+
+public class Stack {
+	
+	private int[] data;
+	private int index;
+	
+	// Konstruktor, liefert einen leeren Stapel mit der Maximalhoehe l.
+	public Stack(int l) { 
+		data = new int [l];
+		index = 0;
+	}
+
+	// Falls der Stapel voll ist, wird false zurueckgemeldet, andernfalls 
+	// wird die Zahl i hinzugefuegt und true zurueckgegeben.
+	public boolean push(int i) 
+	{
+		if (index >= data.length) return false;
+		data[index++] = i;
+		return true;
+	}
+	
+	// Die zuletzt hinzugefuegte Zahl wird entfernt und zurueckgegeben. 
+	// Ist keine Zahl auf dem Stapel, so ist der Rueckgabewert undefiniert.
+	public int pop() {
+		if (empty()) return 0;
+		return data[--index]; 
+	}
+	
+	// Falls der Stapel leer ist, wird true zurueckgeliefert, sonst false.
+	public boolean empty() { 
+		return (index == 0); 
+	} 
+	
+}
